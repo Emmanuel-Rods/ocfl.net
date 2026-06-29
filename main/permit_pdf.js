75d3a8378a0d119688a755f1b70e7c3ec342bf49
Roger Rodrigues <alewsor@gmail.com>
2026-06-29 11:42:58 +0530

    added try catch

--- main/permit_pdf.js
+++ main/permit_pdf.js
@@ -7,13 +7,17 @@ const { extractTextFromLocalPDF } = require("../pdf_processors/pdf_parser.js");
 const { parsePermitData } = require("../pdf_processors/formater.js");
 
 async function permitPDF(rsn) {
-  const html = await getAttachmentHtml(rsn);
-  const payload = buildBasicPayload(html);
-  payload["hdnAttachmentRSN"] = rsn;
-  const pdfbuffer = await downloadPdfFromForm(payload);
-  const pdftext = await extractTextFromLocalPDF(pdfbuffer);
-  const PDFJSON = parsePermitData(pdftext);
-  return PDFJSON;
+  try {
+    const html = await getAttachmentHtml(rsn);
+    const payload = buildBasicPayload(html);
+    payload["hdnAttachmentRSN"] = rsn;
+    const pdfbuffer = await downloadPdfFromForm(payload);
+    const pdftext = await extractTextFromLocalPDF(pdfbuffer);
+    const PDFJSON = parsePermitData(pdftext);
+    return PDFJSON;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = { permitPDF };
